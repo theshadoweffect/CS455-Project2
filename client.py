4f6a66064bd0269c3f1266a25dc9a81a6e885fd9
theshadoweffect <mcguiganriley@gmail.com>
2018-10-25 20:46:57 -0700

    updated Client.py
    Needs testing

--- client.py
+++ client.py
@@ -3,43 +3,64 @@ import sys, timeit
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(("10.0.0.2", 9999))
 s.settimeout(0.5)
+seq = 0
+curWindow = 5
+N = 5
 for i in xrange(0, 10, 1):
-    data = str(i) * 1024 + '\n'
-    s.send(data)
-    ack = False
-    print "Loop 1 sending", len(data), "bytes..."
-    print "Loop 1 waiting for acknowledgement"
-    while not ack:
-        try:
-	    ACK, addr = s.recvfrom(1024)
-            print ("Acknowledgement recieved", ACK)
-            ack = True
-        except timeout:
-	    s.send(data)
+    if seq < curWindow:
+        data = str(seq) + str(i) * 1024 + '\n'
+        s.send(data)
+        print "Loop 1 sending", len(data), "bytes..."
+        seq = seq + 1
+    recieved = False
+    else:
+        while not recieved:	
+            try:
+                ACK, addr = s.recvfrom(1024)
+                print ("Acknowledgement recieved", ACK)
+	        curWindow = N + ACK
+	        seq = ACK + 1
+		i = seq
+	        recieved = True
+            except timeout:
+		seq = curWindow - N
+		i = seq
 for i in xrange(ord('a'), ord('z') + 1, 1):
-    data = chr(i) * 1024 + '\n'
-    s.send(data)
-    print "Loop 2 sending", len(data), "bytes..."
-    print "Loop 2 waiting for acknowledgement"
-    ack = False
-    while not ack:
-        try:
-	    ACK, addr = s.recvfrom(1024)
-            print ("Acknowledgement recieved", ACK)
-            ack = True
-        except timeout:
-	    s.send(data)
+    if seq < curWindow:
+        data = str(seq) + chr(i) * 1024 + '\n'
+        s.send(data)
+        print "Loop 1 sending", len(data), "bytes..."
+        seq = seq + 1
+    recieved = False
+    else:
+        while not recieved:	
+            try:
+                ACK, addr = s.recvfrom(1024)
+                print ("Acknowledgement recieved", ACK)
+	        curWindow = N + ACK
+	        seq = ACK + 1
+		i = seq
+	        recieved = True
+            except timeout:
+		seq = curWindow - N
+		i = seq
 for i in xrange(ord('A'), ord('Z') + 1, 1):
-    data = chr(i) * 1024 + '\n'
-    s.send(data)
-    ack = False
-    print "Loop 3 sending", len(data), "bytes..."
-    print "Loop 3 waiting for acknowledgement"
-    while not ack:
-        try:
-	    ACK, addr = s.recvfrom(1024)
-            print ("Acknowledgement recieved", ACK)
-            ack = True
-        except timeout:
-	    s.send(data)
+    if seq < curWindow:
+        data = str(seq) + chr(i) * 1024 + '\n'
+        s.send(data)
+        print "Loop 1 sending", len(data), "bytes..."
+        seq = seq + 1
+    recieved = False
+    else:
+        while not recieved:	
+            try:
+                ACK, addr = s.recvfrom(1024)
+                print ("Acknowledgement recieved", ACK)
+	        curWindow = N + ACK
+	        seq = ACK + 1
+		i = seq
+	        recieved = True
+            except timeout:
+		seq = curWindow - N
+		i = seq
 s.close()
