64cc94ddd5799deddc1704381d9db1d20dabe1aa
theshadoweffect <mcguiganriley@gmail.com>
2018-10-26 18:05:57 -0700

    Update server.py

--- server.py
+++ server.py
@@ -7,28 +7,29 @@ conn, addr = s.accept()
 start_time = timeit.default_timer()
 f = open("output.txt", "w")
 print('Connected by', addr)
-buf = 10240
+buf = 1024
 data, addr = conn.recvfrom(buf)
 prevsequence = -1
 packets = 1
 while(data):
     conn.settimeout(10)
+    seq, addr = conn.recvfrom(3)
     data, addr = conn.recvfrom(buf)
     print len(data), "bytes received..."
     packets = packets + 1
-    if len(data) <= 1028 and len(data) >= 1027:
-        seqNum, data = data.split(":")
-        print "Receiving packet", seqNum
-        if prevsequence + 1 == int(seqNum):
-            prevsequence = int(seqNum)
-            f.write(data)
+    seqNum = seq.split(":")
+    print "Receiving packet", seqNum
+    if prevsequence + 1 == int(seqNum):
+        prevsequence = int(seqNum)
+        f.write(data)
     conn.send((str(prevsequence)+":")) 
     print "Sending acknowledgement", prevsequence
-droprate =  100 - (100 * 62/packets)  
-print "Packet loss rate", droprate, "%"
+
 conn.close()
 s.close()
 f.close()
+droprate =  100 - (100 * 62/packets)  
+print "Packet loss rate", droprate, "%"
 elapsed = (timeit.default_timer() - start_time)
 throughput = (timeit.default_timer() - start_time)/packets
 print '%.3f seconds' % elapsed
