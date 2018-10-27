d8e5546dc5eac37cb69b65e9ba362acad79cba92
theshadoweffect <mcguiganriley@gmail.com>
2018-10-26 17:59:11 -0700

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,6 @@ s.connect(("10.0.0.2", 9999))
 timeInterval = 3
 s.settimeout(timeInterval)
 seq = 0
-maxSent = 0
 lastACK = 0
 curWindow = 5
 N = 5
@@ -15,17 +14,15 @@ while seq <= 62:
         data = str(seq) + str(a) + str(1) * 1024 + '\n'
         s.send(data)
         print "Sequence: ", seq, "sending", len(data), "bytes..."
-	if seq > maxSent:
-	    maxSent = seq
         seq = seq + 1
-    try:
-        ACK, addr = s.recvfrom(1024)
-        print ("Acknowledgement recieved", ACK)
-	ACK = int(ACK)
-	if ACK > lastACK:
-		lastACK = ACK
-	curWindow = N + lastACK
-	seq = lastACK + 1
-    except timeout:
-	seq = curWindow - N
+    if seq == curWindow:
+        try:
+            data, addr = s.recvfrom(1024)
+            print ("Acknowledgement recieved", ACK)
+	    array = data.split(":")
+	    ACK = int(array[-1:])
+	    curWindow = N + ACK
+	    seq = ACK+1
+        except timeout:
+	    seq = curWindow - N
 s.close()
